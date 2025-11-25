499bce6933edff7a806a00be5bc5c2611fa0a5f5
LorenzoCF7 <lorenzocruzfdez2@gmail.com>
2025-11-25 19:07:39 +0100

    hibernate avances

--- src/main/java/ilcaminodelamamma/model/Ingrediente.java
+++ src/main/java/ilcaminodelamamma/model/Ingrediente.java
@@ -1,8 +1,55 @@
 package ilcaminodelamamma.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ingredientes")
 public class Ingrediente {
-    private int id_ingrediente;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_ingrediente;
     private String nombre;
     private String unidad_medida;
-    private int cantidad_stock;
+    private Integer cantidad_stock;
+
+    public Ingrediente() {}
+
+    public Ingrediente(String nombre, String unidad_medida, Integer cantidad_stock) {
+        this.nombre = nombre;
+        this.unidad_medida = unidad_medida;
+        this.cantidad_stock = cantidad_stock;
+    }
+
+    public Integer getId_ingrediente() {
+        return id_ingrediente;
+    }
+
+    public void setId_ingrediente(Integer id_ingrediente) {
+        this.id_ingrediente = id_ingrediente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUnidad_medida() {
+        return unidad_medida;
+    }
+
+    public void setUnidad_medida(String unidad_medida) {
+        this.unidad_medida = unidad_medida;
+    }
+
+    public Integer getCantidad_stock() {
+        return cantidad_stock;
+    }
+
+    public void setCantidad_stock(Integer cantidad_stock) {
+        this.cantidad_stock = cantidad_stock;
+    }
 }
